ebf702a5ac224199c0fa891e993985c2d4ef3790
Leonardo Rodrigues <leonardo.rodrigues@ibm.com>
2022-01-10 16:26:49 -0300

    Navigation buttons

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import MovieScore from '../MovieScore';
 
 
@@ -11,14 +12,16 @@ export default function MovieCard() {
         score: 4.5
     };
 
-    
+
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
                 <MovieScore />
-                <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+                <Link to={`/form/${movie.id}`}>
+                    <div className="btn btn-primary dsmovie-btn" >Avaliar</div>
+                </Link>
             </div>
         </div>
     );
